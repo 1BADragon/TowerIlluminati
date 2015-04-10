e4f29d0908ada2a0835cfe7e3ebe9e05315c2a18
Ranger Adams <adam6860@vandals.uidaho.edu>
2015-04-09 17:20:42 -0700

    Added Check on Open File Cancel

--- TowerLights2/mainwindow.cpp
+++ TowerLights2/mainwindow.cpp
@@ -157,218 +157,225 @@ void MainWindow::on_actionNew_triggered()
 //this function is called when file>>open is selected
 void MainWindow::on_actionOpen_triggered()
 {
-    delete currentMovie;
-    currentMovie = new Movie();
     fileName = QFileDialog::getOpenFileName(this,
                                             tr("Open File"), "/home/", tr("Tan Files (*.tan *.tan2)"));
-    int version = 0;
-    int count = 1;
-    int tracker = 6;
-    int frameCount = 0;
-    int gridLine = 0;
-
-    int skip = 0;
-    QFile inputFile(fileName);
-    if (inputFile.open(QIODevice::ReadOnly))
+    if (fileName == "")
     {
-        QTextStream in(&inputFile);
-        while (!in.atEnd())
+
+    }
+    else
+    {
+        delete currentMovie;
+        currentMovie= new Movie();
+        int version = 0;
+        int count = 1;
+        int tracker = 6;
+        int frameCount = 0;
+        int gridLine = 0;
+
+        int skip = 0;
+        QFile inputFile(fileName);
+        if (inputFile.open(QIODevice::ReadOnly))
         {
-            skip = 0;
-            QString line = in.readLine();
-            QRegExp rx("[ :.]");
-            QStringList list = line.split(rx, QString::SkipEmptyParts);
-            QList <int> nums;
-            for(int i = 0; i < list.size(); i++)
+            QTextStream in(&inputFile);
+            while (!in.atEnd())
             {
-                nums.append(list.at(i).toInt());
-            }
-
-            //Get version number
-            if(count == 1)
-            {
-                if(line == "0.3")
+                skip = 0;
+                QString line = in.readLine();
+                QRegExp rx("[ :.]");
+                QStringList list = line.split(rx, QString::SkipEmptyParts);
+                QList <int> nums;
+                for(int i = 0; i < list.size(); i++)
                 {
-                    version = 3;
-                    //std::cout<< "Version 3" <<std::endl;
+                    nums.append(list.at(i).toInt());
                 }
-                if(line == "0.4")
+
+                //Get version number
+                if(count == 1)
                 {
-                    version = 4;
-                    //std::cout<< "Version 4" <<std::endl;
-                }
-            }
-
-            //Get audio filename
-            if(count == 2 && line != "NoAudioFile" && version == 4)
-            {
-                currentMovie->setAudio(line);
-            }
-
-            //get current color rgb values for version 3
-            if(count == 2 && version == 3)
-            {
-                red -> setValue(0);
-                green -> setValue(0);
-                blue -> setValue(0);
-                red -> setValue(nums.at(0));
-                green -> setValue(nums.at(1));
-                blue -> setValue(nums.at(2));
-            }
-
-
-            //get current color rgb values for version 4
-            if(count == 3 && version == 4)
-            {
-                red -> setValue(0);
-                green -> setValue(0);
-                blue -> setValue(0);
-                red -> setValue(nums.at(0));
-                green -> setValue(nums.at(1));
-                blue -> setValue(nums.at(2));
-            }
-
-            //Color Pallet RGB values
-            if(count == 4 )
-            {
-                int counter = 0;
-                for(int i = 0; i < 2; i++)
-                {
-                    for(int j = 0; j < 8; j++)
+                    if(line == "0.3")
                     {
-                        QColor tempColor;
-                        tempColor.setRed(nums.at(counter));
-                        counter++;
-                        tempColor.setGreen(nums.at(counter));
-                        counter++;
-                        tempColor.setBlue(nums.at(counter));
-                        counter++;
-                        colorPallet[i][j]->setProperty("color", tempColor);
+                        version = 3;
+                        //std::cout<< "Version 3" <<std::endl;
+                    }
+                    if(line == "0.4")
+                    {
+                        version = 4;
+                        //std::cout<< "Version 4" <<std::endl;
                     }
                 }
-            }
 
-            //Frame count
-            if(count == 5 && version == 4)
-            {
-                /*Ignore for now since there is no frame count variable
-               * ...we are currently just using frameSequence -> length();
-              */
-            }
-
-            //Add timestamp to frames for version 4
-            if(count == tracker  && version == 4)
-            {
-                currentMovie->newFrame();
-                currentMovie->getFrame(frameCount)->setTimeStamp(list.at(0).toInt());
-                tracker = tracker + 21;
-                frameCount++;
-                gridLine = 0;
-                skip = 1;
-            }
-
-            //Add timestamp to frames for version 3
-            if(count == tracker  && version == 3)
-            {
-                qint64 tempTime;
-                int minutes = list.at(0).toInt();
-                int seconds = list.at(1).toInt();
-                int millisec = list.at(2).toInt();
-                //std::cout << minutes << " " << seconds << " " << millisec << std::endl;
-                tempTime = minutes*(1000*60) + seconds*1000 + millisec;
-                currentMovie->newFrame();
-                currentMovie->getFrame(frameCount)->setTimeStamp(tempTime);
-                tracker = tracker + 11;
-                frameCount++;
-                gridLine = 0;
-                skip = 1;
-            }
-
-            //Add Color info to frames version 4
-            if(count < tracker && count > 6 && skip == 0 && version == 4)
-            {
-                int counter = 0;
-                for(int i = 0; i < 12; i++)
+                //Get audio filename
+                if(count == 2 && line != "NoAudioFile" && version == 4)
                 {
-                    int tempRed = nums.at(counter);
-                    int tempGreen = nums.at(counter + 1);
-                    int tempBlue = nums.at(counter + 2);
-                    if(tempRed == 0 && tempGreen == 0 && tempBlue == 0)
-                    {
-                        tempRed = 128;
-                        tempGreen = 128;
-                        tempBlue = 128;
-                    }
-                    currentMovie->getFrame(frameCount - 1)->FullGridPixel(gridLine, i)
-                            ->setColor(tempRed,tempGreen,tempBlue);
-                    /*
-                  if(gridLine > 4 && gridLine < 15 && i > 3 && i < 8)
-                  {
-                      currentMovie->getFrame(frameCount - 1)->TowerGridPixel(gridLine-5, i-4)
-                              ->setColor(tempRed,tempGreen,tempBlue);
-                  }
-                  */
-                    counter = counter + 3;
+                    currentMovie->setAudio(line);
                 }
-                gridLine++;
-            }
 
-            //Add Color info to frames version 3
-            if(count < tracker && count > 6 && skip == 0 && version == 3)
-            {
-                int tempRed;
-                int tempGreen;
-                int tempBlue;
-                //Populate first rows of full grid before reading more lines
-                if(gridLine < 5)
+                //get current color rgb values for version 3
+                if(count == 2 && version == 3)
                 {
-                    while(gridLine < 5 )
+                    red -> setValue(0);
+                    green -> setValue(0);
+                    blue -> setValue(0);
+                    red -> setValue(nums.at(0));
+                    green -> setValue(nums.at(1));
+                    blue -> setValue(nums.at(2));
+                }
+
+
+                //get current color rgb values for version 4
+                if(count == 3 && version == 4)
+                {
+                    red -> setValue(0);
+                    green -> setValue(0);
+                    blue -> setValue(0);
+                    red -> setValue(nums.at(0));
+                    green -> setValue(nums.at(1));
+                    blue -> setValue(nums.at(2));
+                }
+
+                //Color Pallet RGB values
+                if(count == 4 )
+                {
+                    int counter = 0;
+                    for(int i = 0; i < 2; i++)
                     {
-                        for(int i = 0; i < 12; i++)
+                        for(int j = 0; j < 8; j++)
                         {
-                            tempRed = 128;
-                            tempGreen = 128;
-                            tempBlue = 128;
-                            currentMovie->getFrame(frameCount - 1)->FullGridPixel(gridLine, i)
-                                    ->setColor(tempRed,tempGreen,tempBlue);
+                            QColor tempColor;
+                            tempColor.setRed(nums.at(counter));
+                            counter++;
+                            tempColor.setGreen(nums.at(counter));
+                            counter++;
+                            tempColor.setBlue(nums.at(counter));
+                            counter++;
+                            colorPallet[i][j]->setProperty("color", tempColor);
                         }
-                        gridLine++;
                     }
                 }
-                int counter = 0;
-                for(int i = 0; i < 12; i++)
+
+                //Frame count
+                if(count == 5 && version == 4)
                 {
-                    if(i < 4 || i > 7 || gridLine > 14)
+                    /*Ignore for now since there is no frame count variable
+                   * ...we are currently just using frameSequence -> length();
+                  */
+                }
+
+                //Add timestamp to frames for version 4
+                if(count == tracker  && version == 4)
+                {
+                    currentMovie->newFrame();
+                    currentMovie->getFrame(frameCount)->setTimeStamp(list.at(0).toInt());
+                    tracker = tracker + 21;
+                    frameCount++;
+                    gridLine = 0;
+                    skip = 1;
+                }
+
+                //Add timestamp to frames for version 3
+                if(count == tracker  && version == 3)
+                {
+                    qint64 tempTime;
+                    int minutes = list.at(0).toInt();
+                    int seconds = list.at(1).toInt();
+                    int millisec = list.at(2).toInt();
+                    //std::cout << minutes << " " << seconds << " " << millisec << std::endl;
+                    tempTime = minutes*(1000*60) + seconds*1000 + millisec;
+                    currentMovie->newFrame();
+                    currentMovie->getFrame(frameCount)->setTimeStamp(tempTime);
+                    tracker = tracker + 11;
+                    frameCount++;
+                    gridLine = 0;
+                    skip = 1;
+                }
+
+                //Add Color info to frames version 4
+                if(count < tracker && count > 6 && skip == 0 && version == 4)
+                {
+                    int counter = 0;
+                    for(int i = 0; i < 12; i++)
                     {
-                        tempRed = 128;
-                        tempGreen = 128;
-                        tempBlue = 128;
-                    }
-                    else
-                    {
-                        tempRed = nums.at(counter);
-                        tempGreen = nums.at(counter + 1);
-                        tempBlue = nums.at(counter + 2);
+                        int tempRed = nums.at(counter);
+                        int tempGreen = nums.at(counter + 1);
+                        int tempBlue = nums.at(counter + 2);
                         if(tempRed == 0 && tempGreen == 0 && tempBlue == 0)
                         {
                             tempRed = 128;
                             tempGreen = 128;
                             tempBlue = 128;
                         }
+                        currentMovie->getFrame(frameCount - 1)->FullGridPixel(gridLine, i)
+                                ->setColor(tempRed,tempGreen,tempBlue);
+                        /*
+                      if(gridLine > 4 && gridLine < 15 && i > 3 && i < 8)
+                      {
+                          currentMovie->getFrame(frameCount - 1)->TowerGridPixel(gridLine-5, i-4)
+                                  ->setColor(tempRed,tempGreen,tempBlue);
+                      }
+                      */
                         counter = counter + 3;
                     }
-                    currentMovie->getFrame(frameCount - 1)->FullGridPixel(gridLine, i)
-                            ->setColor(tempRed,tempGreen,tempBlue);
+                    gridLine++;
                 }
-                gridLine++;
-            }
 
-            count++;
+                //Add Color info to frames version 3
+                if(count < tracker && count > 6 && skip == 0 && version == 3)
+                {
+                    int tempRed;
+                    int tempGreen;
+                    int tempBlue;
+                    //Populate first rows of full grid before reading more lines
+                    if(gridLine < 5)
+                    {
+                        while(gridLine < 5 )
+                        {
+                            for(int i = 0; i < 12; i++)
+                            {
+                                tempRed = 128;
+                                tempGreen = 128;
+                                tempBlue = 128;
+                                currentMovie->getFrame(frameCount - 1)->FullGridPixel(gridLine, i)
+                                        ->setColor(tempRed,tempGreen,tempBlue);
+                            }
+                            gridLine++;
+                        }
+                    }
+                    int counter = 0;
+                    for(int i = 0; i < 12; i++)
+                    {
+                        if(i < 4 || i > 7 || gridLine > 14)
+                        {
+                            tempRed = 128;
+                            tempGreen = 128;
+                            tempBlue = 128;
+                        }
+                        else
+                        {
+                            tempRed = nums.at(counter);
+                            tempGreen = nums.at(counter + 1);
+                            tempBlue = nums.at(counter + 2);
+                            if(tempRed == 0 && tempGreen == 0 && tempBlue == 0)
+                            {
+                                tempRed = 128;
+                                tempGreen = 128;
+                                tempBlue = 128;
+                            }
+                            counter = counter + 3;
+                        }
+                        currentMovie->getFrame(frameCount - 1)->FullGridPixel(gridLine, i)
+                                ->setColor(tempRed,tempGreen,tempBlue);
+                    }
+                    gridLine++;
+                }
+
+                count++;
+            }
+            inputFile.close();
         }
-        inputFile.close();
+        updateMainTower();
+        updateUI();
     }
-    updateMainTower();
-    updateUI();
 }
 
 //this function is called when file>>export is selected
